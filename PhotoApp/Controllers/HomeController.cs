8dbac53272ecc672babbef6dfcaa4c378fc08217
Simrn Puri <purisimran13@gmail.com>
2021-08-09 17:55:34 -0400

    updates

--- PhotoApp/Controllers/HomeController.cs
+++ PhotoApp/Controllers/HomeController.cs
@@ -1,37 +1,325 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PhotoApp.Data;
 using PhotoApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.IO;
 
 namespace PhotoApp.Controllers
 {
-    public class HomeController : Controller
+    [Authorize]
+    public class CommentsController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly InstagramContext _context;
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public CommentsController(
+            PhotoAppContext context,
+            UserManager<User> userManager,
+            SignInManager<User> signInManager)
         {
-            _logger = logger;
+            _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
 
-        public IActionResult Index()
+        // GET: Comments
+        public async Task<IActionResult> Index()
         {
+            var instagramContext = _context.Comments.Include(c => c.Post).Include(c => c.User);
+            return View(await instagramContext.ToListAsync());
+        }
+
+        // GET: Comments/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var comment = await _context.Comments
+                .Include(c => c.Post)
+                .Include(c => c.User)
+                .Include(c => c.CommentLikes).ThenInclude(cl => cl.User)
+                .AsNoTracking()
+                .SingleOrDefaultAsync(m => m.ID == id);
+            if (comment == null)
+            {
+                return NotFound();
+            }
+
+            return View(comment);
+        }
+
+        public async Task<IActionResult> Like(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Comment comment = null;
+            CommentLike commentlike = null;
+            try
+            {
+                var user = await _userManager.GetUserAsync(User);
+
+                comment = await _context.Comments
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync(m => m.ID == id);
+
+                commentlike = new CommentLike
+                {
+                    UserID = await _userManager.GetUserIdAsync(user),
+                    CommentID = comment.ID
+                };
+
+                if (ModelState.IsValid)
+                {
+                    if (_context.CommentLikes.Contains(commentlike))
+                    {
+                        _context.Remove(commentlike);
+                        await _context.SaveChangesAsync();
+                        return RedirectToAction(nameof(Index));
+                    }
+                    _context.Add(commentlike);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (DbUpdateException /* ex */)
+            {
+                //Log the error (uncomment ex variable name and write a log.
+                ModelState.AddModelError("", "Unable to save changes. " +
+                    "Try again, and if the problem persists " +
+                    "see your system administrator.");
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Comments/Create
+        public IActionResult Create()
+        {
+            ViewData["PostID"] = new SelectList(_context.Posts, "ID", "ID");
             return View();
         }
 
-        public IActionResult Privacy()
+        // POST: Comments/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PostID,Content")] CommentViewModel commentViewModel)
         {
-            return View();
+            Comment comment = null;
+            try
+            {
+                var user = await _userManager.GetUserAsync(User);
+
+                comment = new Comment
+                {
+                    UserID = await _userManager.GetUserIdAsync(user),
+                    Content = commentViewModel.Content,
+                    PostID = commentViewModel.PostID,
+                    CommentTime = DateTime.Now
+                };
+
+                var errors = ModelState
+    .Where(x => x.Value.Errors.Count > 0)
+    .Select(x => new { x.Key, x.Value.Errors })
+    .ToArray();
+
+                if (ModelState.IsValid)
+                {
+                    _context.Add(comment);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                ViewData["PostID"] = new SelectList(_context.Posts, "ID", "ID", commentViewModel.PostID);
+            }
+            catch (DbUpdateException /* ex */)
+            {
+                //Log the error (uncomment ex variable name and write a log.
+                ModelState.AddModelError("", "Unable to save changes. " +
+                    "Try again, and if the problem persists " +
+                    "see your system administrator.");
+            }
+            return View(commentViewModel);
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        [HttpGet]
+        public async Task<IActionResult> ViewPostImage(int? id)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var comment = await _context.Comments
+                .Include(p => p.User)
+                .Include(p => p.Post)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.ID == id);
+
+            if (comment == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    return File(comment.Post.Image, "image/png");
+                }
+            }
+            catch (DbUpdateException /* ex */)
+            {
+                //Log the error (uncomment ex variable name and write a log.
+                ModelState.AddModelError("", "Unable to save changes. " +
+                    "Try again, and if the problem persists " +
+                    "see your system administrator.");
+            }
+            return NotFound();
+        }
+
+        // GET: Comments/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var comment = await _context.Comments.FindAsync(id);
+            if (comment == null)
+            {
+                return NotFound();
+            }
+
+            if (comment.UserID != await _userManager.GetUserIdAsync(user))
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewData["PostID"] = new SelectList(_context.Posts, "ID", "ID", comment.PostID);
+            return View(comment);
+        }
+
+        // POST: Comments/Edit/5
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditComment(int? id)
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var CommentToUpdate = await _context.Comments.SingleOrDefaultAsync(c => c.ID == id);
+
+            if (CommentToUpdate.UserID != await _userManager.GetUserIdAsync(user))
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            if (await TryUpdateModelAsync<Comment>(
+                CommentToUpdate,
+                "",
+                c => c.Content))
+            {
+                try
+                {
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException /* ex */)
+                {
+                    //Log the error (uncomment ex variable name and write a log.)
+                    ModelState.AddModelError("", "Unable to save changes. " +
+                        "Try again, and if the problem persists, " +
+                        "see your system administrator.");
+                }
+            }
+            ViewData["PostID"] = new SelectList(_context.Posts, "ID", "ID", CommentToUpdate.PostID);
+            return View(CommentToUpdate);
+        }
+
+        // GET: Comments/Delete/5
+        public async Task<IActionResult> Delete(int? id, bool? saveChangesError = false)
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var comment = await _context.Comments
+                .Include(c => c.Post)
+                .Include(c => c.User)
+                .AsNoTracking()
+                .SingleOrDefaultAsync(m => m.ID == id);
+            if (comment == null)
+            {
+                return NotFound();
+            }
+
+            if (comment.UserID != await _userManager.GetUserIdAsync(user))
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewData["ErrorMessage"] =
+                    "Delete failed. Try again, and if the problem persists " +
+                    "see your system administrator.";
+            }
+            return View(comment);
+        }
+
+        // POST: Comments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            var comment = await _context.Comments
+                .AsNoTracking()
+                .SingleOrDefaultAsync(m => m.ID == id);
+            if (comment == null || comment.UserID != await _userManager.GetUserIdAsync(user))
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            try
+            {
+                _context.Comments.Remove(comment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (DbUpdateException /* ex */)
+            {
+                //Log the error (uncomment ex variable name and write a log.)
+                return RedirectToAction(nameof(Delete), new { id, saveChangesError = true });
+            }
+        }
+
+        private bool CommentExists(int id)
+        {
+            return _context.Comments.Any(e => e.ID == id);
         }
     }
 }
---
+++ PhotoApp/Data/PhotoAppContext.cs
@@ -0,0 +1,112 @@
+﻿using PhotoApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PhotoApp.Data
+{
+    public class PhotoAppContext : IdentityDbContext
+    {
+        public PhotoAppContext(DbContextOptions<PhotoAppContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> MyUsers { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<PostLike> PostLikes { get; set; }
+        public DbSet<CommentLike> CommentLikes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("MyUsers");
+            modelBuilder.Entity<Post>().ToTable("Post");
+            modelBuilder.Entity<Comment>().ToTable("Comment");
+            modelBuilder.Entity<PostLike>().ToTable("PostLike");
+            modelBuilder.Entity<CommentLike>().ToTable("CommentLike");
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+
+            modelBuilder.Entity<PostLike>().HasKey(p => new { p.UserID, p.PostID });
+            modelBuilder.Entity<CommentLike>().HasKey(c => new { c.UserID, c.CommentID });
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override int SaveChanges()
+        {
+            //LowerCaseUsername();
+            AddTimestamps();
+            return base.SaveChanges();
+        }
+
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            //LowerCaseUsername();
+            AddTimestamps();
+            return base.SaveChanges(acceptAllChangesOnSuccess);
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            //LowerCaseUsername();
+            AddTimestamps();
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            //LowerCaseUsername();
+            AddTimestamps();
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        private void AddTimestamps()
+        {
+            var postEntities = ChangeTracker.Entries().Where(x => x.Entity is Post && (x.State == EntityState.Added));
+
+            var commentEntities = ChangeTracker.Entries().Where(x => x.Entity is Comment && (x.State == EntityState.Added));
+
+            foreach (var entity in postEntities)
+            {
+                ((Post)entity.Entity).PostTime = DateTime.Now;
+            }
+
+            foreach (var entity in commentEntities)
+            {
+                ((Comment)entity.Entity).CommentTime = DateTime.Now;
+            }
+        }
+
+        private void LowerCaseUsername()
+        {
+            var userEntities = ChangeTracker.Entries().Where(x => x.Entity is User && (x.State == EntityState.Added));
+
+            foreach (var entity in userEntities)
+            {
+                ((User)entity.Entity).UserName = ((User)entity.Entity).UserName.ToLowerInvariant();
+            }
+        }
+
+        public bool VerifyUsername(string username)
+        {
+            var userEntities = this.Users;
+            foreach (var user in userEntities)
+            {
+                if (user.UserName.Equals(username.ToLowerInvariant()))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
+
